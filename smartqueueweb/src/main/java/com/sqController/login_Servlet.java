0a3a5da698afe855998691193ce38793b22af7ba
AyelDev <aarielabelgas23@gmail.com>
2024-06-15 15:39:33 +0800

    adding css file

--- smartqueueweb/src/main/java/com/sqController/login_Servlet.java
+++ smartqueueweb/src/main/java/com/sqController/login_Servlet.java
@@ -1,41 +1,28 @@
 package com.sqController;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class login_Servlet
- */
 @WebServlet("/login_Servlet")
 public class login_Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public login_Servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userName = request.getParameter("txtUsername");
+		String passWord = request.getParameter("txtPassword");
+		PrintWriter out = response.getWriter();
+		if(userName.equals("user") & passWord.equals("pass")) {
+			out.print("login success");
+		}else {
+			out.print("login failed");
+		}
 	}
 
 }
--- smartqueueweb/src/main/webapp/login.jsp
+++ smartqueueweb/src/main/webapp/login.jsp
@@ -1,12 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="style.css">
 <meta charset="UTF-8">
 <title>login page</title>
 </head>
 <body>
-
+	<form action="login_Servlet" method="post">
+	<label>Username</label>
+	<input type="text" name="txtUsername">
+	<label>Password</label>
+	<input type="password" name="txtPassword">
+	<input type="submit" value="login">
+	</form>
 </body>
 </html>
