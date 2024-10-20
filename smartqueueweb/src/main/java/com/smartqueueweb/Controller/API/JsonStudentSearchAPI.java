e06e8be425685311fbe4600ea3083d6a2deb216a
AyelDev <aarielabelgas23@gmail.com>
2024-10-20 13:30:21 +0800

    student search api servlet

---
+++ smartqueueweb/src/main/java/com/smartqueueweb/Controller/API/JsonStudentSearchAPI.java
@@ -0,0 +1,38 @@
+package com.smartqueueweb.Controller.API;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.smartqueueweb.Model.StaffBean;
+import com.smartqueueweb.Service.ServiceImpl;
+
+@WebServlet("/JsonStudentSearchAPI")
+public class JsonStudentSearchAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	ServiceImpl services = new ServiceImpl();
+	RequestDispatcher rd = null;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Gson gson = new Gson();
+
+		List<StaffBean> ss = services.listsOfStaff();
+		String userJson = gson.toJson(ss);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+
+		PrintWriter out = response.getWriter();
+		out.print(userJson);
+		out.close();
+	}
+
+}
