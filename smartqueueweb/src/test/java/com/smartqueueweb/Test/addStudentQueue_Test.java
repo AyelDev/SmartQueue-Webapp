ef07dac2a9604ff28a069c963146eb4de9cf7a49
AyelDev <aarielabelgas23@gmail.com>
2024-11-17 19:16:48 +0800

    added Mock Tests for Unit Testing

--- smartqueueweb/src/test/java/com/smartqueueweb/Test/addStudentQueue_Test.java
+++ smartqueueweb/src/test/java/com/smartqueueweb/Test/addStudentQueue_Test.java
@@ -1,29 +1,42 @@
 package com.smartqueueweb.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.smartqueueweb.Service.ServiceImpl;
 
 public class addStudentQueue_Test {
-	ServiceImpl service = new ServiceImpl();
-
-	@Test
-	public void testIfAllSampleFilled() {
-
-		int expected = 1;
-		int actual = service.addToQueue("152G", 12313, "cindy labra", "enrollment", "records");
-
-		assertEquals(expected, actual);
-
+	
+	private ServiceImpl mockServiceImpl;
+	
+	@Before
+	public void setup() {
+	mockServiceImpl = mock(ServiceImpl.class);
 	}
 
 	@Test
-	public void testIfSomeSampleNotFilled() {
+	public void filledSampleShouldReturnProperly() {
 
+		when(mockServiceImpl.addToQueue("152G", 12313, "cindy labra", "enrollment", "records")).thenReturn(1);
+		
 		int expected = 1;
-		int actual = service.addToQueue("223G", 3213, "", "enrollment", "records");
+		int actual = mockServiceImpl.addToQueue("152G", 12313, "cindy labra", "enrollment", "records");
+		
+		assertEquals(expected, actual);
+		
+	}
+
+	@Test
+	public void nonFilledSampleShouldReturnProperly() {
+		
+		when(mockServiceImpl.addToQueue("223G", 000, "", "enrollment", "records")).thenReturn(1);
+		
+		int expected = 1;
+		int actual = mockServiceImpl.addToQueue("223G", 000, "", "enrollment", "records");
 
 		assertEquals(expected, actual);
 	}
--- smartqueueweb/src/test/java/com/smartqueueweb/Test/addStudent_Test.java
+++ smartqueueweb/src/test/java/com/smartqueueweb/Test/addStudent_Test.java
@@ -1,21 +1,39 @@
 package com.smartqueueweb.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import com.google.protobuf.Service;
+import com.smartqueueweb.Model.AdminBean;
 import com.smartqueueweb.Service.ServiceImpl;
 
 public class addStudent_Test {
 
+	private AdminBean adminBean;
+	private ServiceImpl mockServiceImpl;
+	
+	@Before
+	public void setup() {
+	
+	mockServiceImpl = mock(ServiceImpl.class);
+	adminBean = new AdminBean();
+//	adminBean.setAdminId(1);
+//	adminBean.setUsername("cecadmin");
+//	adminBean.setPassword("admin123");
+	}
+	
 	@Test
-	public void test() {
+	public void ItShouldAddTheStudent() {
 		
-		ServiceImpl service = new ServiceImpl();
+		when(mockServiceImpl.addStudent(1231231, "ariel", "piodena", "abelgas", "BSIT")).thenReturn(1);
 		
-		int actual = service.addStudent(1231231, "ariel", "piodena", "abelgas", "BSIT");
 		int expected = 1;
+		int actual = mockServiceImpl.addStudent(1231231, "ariel", "piodena", "abelgas", "BSIT");
 		
 		assertEquals(expected, actual);
 	}
